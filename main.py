d4f505f0fdaa9a140d07bcf19168603876d2820b
rogers.machara <rogersmachara13@gmail.com>
2024-11-12 16:53:41 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import pyttsx3
+import speech_recognition as sr
+import datetime
+import os
+import smtplib
+import wikipedia
+import webbrowser
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishme():
+    hour = int(datetime.datetime.now().hour)
+    
